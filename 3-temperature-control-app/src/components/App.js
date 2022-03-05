d7ad94758abef8f241bab16572d7114f53b8eb06
Kruze Zab <kruzezab4@gmail.com>
2022-03-05 09:07:24 +0545

    add temperature controller

---
+++ 3-temperature-control-app/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Temperature from './Temperature';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Temperature />
+    </div>
+  );
+};
+
+export default App;
